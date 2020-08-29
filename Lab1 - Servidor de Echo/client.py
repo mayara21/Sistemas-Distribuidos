8358b77efc57940ce31473960e755809d7e70a37
Mayara <mayara.mpf21@gmail.com>
2020-08-29 01:34:16 -0300

    add comments

--- Lab1 - Servidor de Echo/client.py
+++ Lab1 - Servidor de Echo/client.py
@@ -2,11 +2,14 @@ import socket
 
 HOST: str = 'localhost'
 PORT: int = 5000
-
+    
+# socket initialization (using the default internet address family and stream socket type)  
 socket = socket.socket()
 
+# connects with server
 socket.connect((HOST, PORT))
 
+# waits for user input and transmits it to server until user decides to close the connection
 while True:
     message: str = input()
 
@@ -15,7 +18,9 @@ while True:
     else:
         socket.send(message.encode('utf-8'))
 
+        # receives and prints the echoed message
         received_message = socket.recv(1024)
         print(str(received_message, encoding='utf-8'))
 
+# closes the connection
 socket.close()
