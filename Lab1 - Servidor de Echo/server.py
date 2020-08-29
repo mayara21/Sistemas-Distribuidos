157e6137fbd33c57bdc4a9b20a7843001cd4035c
Mayara <mayara.mpf21@gmail.com>
2020-08-29 01:28:13 -0300

    add comments

--- Lab1 - Servidor de Echo/server.py
+++ Lab1 - Servidor de Echo/server.py
@@ -3,14 +3,17 @@ import socket
 HOST: str = ''
 PORT: int = 5000
 
+# socket initialization (using the default internet address family and stream socket type) and binding
 socket = socket.socket()
-
 socket.bind((HOST, PORT))
 
+# awaits for a connection and stablishes a maximum of 1 pending connection
 socket.listen(1)
 
+# accepts first connection
 newSocket, address = socket.accept()
 
+# keeps the connection and message trading until client decides to close it
 while True:
     message = newSocket.recv(1024)
 
@@ -19,6 +22,6 @@ while True:
     else:
         newSocket.send(message)
 
+# closes connection and main socket
 newSocket.close()
-
 socket.close()
