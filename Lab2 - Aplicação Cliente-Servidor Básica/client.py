f3f7c38250b30b8ecec6782cd7e7cabc5e1d6b38
Mayara Martins <mayara.mpf21@gmail.com>
2020-09-06 01:10:12 -0300

    add comments

--- Lab2 - Aplicação Cliente-Servidor Básica/client.py
+++ Lab2 - Aplicação Cliente-Servidor Básica/client.py
@@ -1,5 +1,4 @@
 import socket
-import json
 
 HOST: str = 'localhost'
 PORT: int = 5000
@@ -10,18 +9,19 @@ socket = socket.socket()
 # connects with server
 socket.connect((HOST, PORT))
 
-print('Connected with server!\nType the text file name you want to analyze in the format \'test.txt\'.\nTo close the connection, send \'close\'.')
+print('Type the text file name you want to analyze in the format \'test.txt\'.\nTo close the connection, send \'close\'.')
 
 # waits for user input and transmits it to server until user decides to close the connection
-
 while True:
     fileName: str = input()
 
+    # check if user wants to close the connection
     if fileName.lower() == "close": 
         break
     else:
         socket.send(fileName.encode('utf-8'))
 
+        # receives answer from server, decodes it and prints it on console
         received_message = socket.recv(4096)
         print(str(received_message, encoding='utf-8'))
 
--- Lab2 - Aplicação Cliente-Servidor Básica/server.py
+++ Lab2 - Aplicação Cliente-Servidor Básica/server.py
@@ -6,10 +6,12 @@ PORT: int = 5000
 ERROR_MESSAGE: str = "File not found."
 TOP_ITENS_SIZE: int = 10
 
+# function for counting the words on file content, using a dictionary
 def text_processing(content: str):
     dictionary = {}
 
     for word in content.lower().split(" "):
+        # removes ponctuation
         word = word.replace(".","").replace(",","").replace(":","").replace("\"","").replace("!","").replace("?","").replace("*","")
         
         if word not in dictionary:
@@ -19,6 +21,7 @@ def text_processing(content: str):
     
     return dictionary
 
+# function for sorting the dictionary in descending order and return a dictionary containing the top n itens
 def sort_top_itens(dictionary: dict):
     top_itens = {}
     sorted_list = sorted(dictionary, key = dictionary.get, reverse = True)
@@ -34,40 +37,45 @@ def main():
     socket = sock.socket()
     socket.bind((HOST, PORT))
 
-    # awaits for a connection and stablishes a maximum of 5 pending connection
-    socket.listen(5)
-
-    # keeps the connection and message trading until client decides to close it
+    # awaits for a connection and stablishes a maximum of 1 pending connection
+    socket.listen(1)
+    
     while True:
         # accepts first connection
         new_socket, address = socket.accept()
 
         print('Connected with: ', address)
 
+        # keeps the connection and message trading until client decides to close it
         while True:
             file_name = new_socket.recv(4096)
 
+            # if client wants to close the connection, breaks the loop
             if not file_name: 
                 break
 
             try:
+                # open and read file, saving the content
                 new_file_name = str(file_name, encoding='utf-8')
                 txt_file = open(new_file_name, 'r', encoding='utf-8')
                 content = txt_file.read()
                 txt_file.close()
                 
+                # counts the words in content and stablishes the top itens
                 dictionary = text_processing(content)
                 top_itens = sort_top_itens(dictionary)
             
+                # serializes the dictionary in a json, and sends it to client
                 data = json.dumps(top_itens, ensure_ascii=False)
                 new_socket.send(data.encode())
 
+            # if file is not found, send an error message to client
             except FileNotFoundError:
                 new_socket.send(ERROR_MESSAGE.encode('utf-8'))
 
         # closes connection socket
         new_socket.close()
-        
+
     socket.close()
 
 if __name__ == "__main__":
