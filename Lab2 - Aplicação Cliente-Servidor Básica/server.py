91d647e9a333be1f24539928e29f823503497f2e
Mayara Martins <mayara.mpf21@gmail.com>
2020-09-05 23:58:31 -0300

    add text processing and sorting

--- Lab2 - Aplicação Cliente-Servidor Básica/server.py
+++ Lab2 - Aplicação Cliente-Servidor Básica/server.py
@@ -1,40 +1,68 @@
-import socket
+import socket as sock
 
 HOST: str = ''
 PORT: int = 5000
-
 ERROR_MESSAGE: str = "File not found."
+TOP_ITENS_SIZE: int = 10
 
-# socket initialization (using the default internet address family and stream socket type) and binding
-socket = socket.socket()
-socket.bind((HOST, PORT))
+def text_processing(content: str):
+    dictionary = {}
 
-# awaits for a connection and stablishes a maximum of 5 pending connection
-socket.listen(5)
-
-# accepts first connection
-newSocket, address = socket.accept()
-
-print('Connected with: ', address)
-
-# keeps the connection and message trading until client decides to close it
-while True:
-    fileName = newSocket.recv(1024)
-
-    if not fileName: 
-        break
-
-    try:
-        txtFile = open(str(fileName, encoding='utf-8'), 'r')
-        content = txtFile.read()
+    for word in content.lower().split(" "):
+        word = word.replace(".","").replace(",","").replace(":","").replace("\"","").replace("!","").replace("?","").replace("*","")
         
-        txtFile.close()
-    except FileNotFoundError:
-        newSocket.send(ERROR_MESSAGE.encode('utf-8'))
+        if word not in dictionary:
+            dictionary[word] = 1
+        else:
+            dictionary[word] += 1
+    
+    return dictionary
 
-    else:
-        newSocket.send(b'tudo certo')
+def sort_top_itens(dictionary: dict):
+    top_itens = {}
+    sorted_list = sorted(dictionary, key = dictionary.get, reverse = True)
 
-# closes connection and main socket
-newSocket.close()
-socket.close()
+    for word in sorted_list[:TOP_ITENS_SIZE]:
+        top_itens[word] = dictionary[word]
+
+    return top_itens
+
+
+def main():
+    # socket initialization (using the default internet address family and stream socket type) and binding
+    socket = sock.socket()
+    socket.bind((HOST, PORT))
+
+    # awaits for a connection and stablishes a maximum of 5 pending connection
+    socket.listen(5)
+
+    # accepts first connection
+    new_socket, address = socket.accept()
+
+    print('Connected with: ', address)
+
+    # keeps the connection and message trading until client decides to close it
+    while True:
+        file_name = new_socket.recv(1024)
+
+        if not file_name: 
+            break
+
+        try:
+            new_file_name = str(file_name, encoding='utf-8')
+            txt_file = open(new_file_name, 'r', encoding='utf-8')
+            content = txt_file.read()
+            dictionary = text_processing(content)
+            top_itens = sort_top_itens(dictionary)
+            print(top_itens)
+            txt_file.close()
+            new_socket.send(b'tudo certo')
+        except FileNotFoundError:
+            new_socket.send(ERROR_MESSAGE.encode('utf-8'))
+
+    # closes connection and main socket
+    new_socket.close()
+    socket.close()
+
+if __name__ == "__main__":
+    main()
