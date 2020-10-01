9242cd5371d01bd074577f1c882e8cd7043c520d
Mayara <mayara.mpf21@gmail.com>
2020-10-01 01:36:11 -0300

    add get, check and list request and responses

--- Lab4 - Bate-papo/server.py
+++ Lab4 - Bate-papo/server.py
@@ -14,7 +14,7 @@ PORT: int = 7000
 ENCODING: str = 'utf-8'
 
 inputs = [sys.stdin]
-user_list = [User('ana', '', 0)]
+user_list = [User('ana', '127.0.0.1', 0), User('bob', '127.0.0.1', 0), User('sofia', '127.0.0.1', 0)]
 
 
 def init_server():
@@ -31,6 +31,7 @@ def init_server():
 
 def accept_conection(socket):
     return socket.accept()
+
 
 def unpack_user_message(msg: bytearray):
     user_ip: str = ''
@@ -49,15 +50,29 @@ def unpack_user_message(msg: bytearray):
 
     return new_user
 
+
+def unpack_get_user_request(msg: bytearray):
+    user_name = str(struct.unpack('=16s', msg[:16])[0], encoding=ENCODING)
+    user_name = user_name.strip('\x00')
+
+    return user_name
+
+def unpack_check_user_request(msg: bytearray):
+    user_name = str(struct.unpack('=16s', msg[:16])[0], encoding=ENCODING)
+    user_name = user_name.strip('\x00')
+
+    return user_name
+
+
 def send_error_response(client_sock, request_type, error_message):
     method_bytes: bytes = struct.pack('=8s', request_type.encode(ENCODING))
     status_bytes: bytes = struct.pack('=B', Status.ERROR.value)
     error_message: bytes = struct.pack('=256s', error_message.encode(ENCODING))
 
     error_response: bytes = method_bytes + status_bytes + error_message
-    print(error_response)
 
     client_sock.sendall(error_response)
+
 
 def send_ok_response(client_sock, request_type):
     method_bytes: bytes = struct.pack('=8s', request_type.encode(ENCODING))
@@ -66,6 +81,24 @@ def send_ok_response(client_sock, request_type):
     ok_response: bytes = method_bytes + status_bytes
 
     client_sock.sendall(ok_response)
+
+
+def send_user_response(client_sock, user, request_type):
+    method_bytes: bytes = struct.pack('=8s', request_type.encode(ENCODING))
+    status_bytes: bytes = struct.pack('=B', Status.OK.value)
+
+    bytes_name: bytes = struct.pack('=16s', user.name.encode(ENCODING))
+
+    bytes_ip: bytes = b''
+    ip_parts: list = user.ip_address.split(".")
+    for part in ip_parts:
+        bytes_ip += struct.pack('=B', int(part))
+    
+    bytes_port: bytes = struct.pack('=H', user.port)
+
+    user_response = method_bytes + status_bytes + bytes_name + bytes_ip + bytes_port
+
+    client_sock.sendall(user_response)
 
 
 def handle_connection_request(client_sock, user):
@@ -77,7 +110,48 @@ def handle_connection_request(client_sock, user):
     
     user_list.append(user)
     send_ok_response(client_sock, Request.ENTER_CHAT.value)
-    print(user_list)
+
+
+def handle_get_user_request(client_sock, name):
+    for member in user_list:
+        if member.name == name:
+            send_user_response(client_sock, member, Request.GET_USER.value)
+            return
+
+    error_message = 'User ' + name + ' is not connect to the chat'
+    send_error_response(client_sock, Request.GET_USER.value, error_message)
+
+
+def send_list_response(client_sock, request_type, simplified_list):
+    list_size = len(simplified_list)
+    method_bytes: bytes = struct.pack('=8s', request_type.encode(ENCODING))
+    status_bytes: bytes = struct.pack('=B', Status.OK.value)
+
+    list_size_bytes: bytes = struct.pack('=H', list_size)
+    names_bytes: bytes = b''
+
+    for i in range(list_size):
+        names_bytes += struct.pack('=16s', simplified_list[i].encode(ENCODING))
+
+    list_response = method_bytes + status_bytes + list_size_bytes + names_bytes
+
+    client_sock.sendall(list_response)
+
+
+def get_simplified_list():
+    simplified_list = []
+    for user in user_list:
+        simplified_list.append(user.name)
+
+    return simplified_list
+
+
+def handle_get_list_request(client_sock):
+    simplified_list = get_simplified_list()
+    send_list_response(client_sock, Request.LIST_USERS.value, simplified_list)
+
+def validate_connection(name):
+    print('Validar a conexao')
 
 
 def serve(client_sock, address):
@@ -85,22 +159,25 @@ def serve(client_sock, address):
         request = client_sock.recv(4096)
 
         if not request:
-            print('> Client ' + str(address) + ' disconnected') 
             client_sock.close()
             return
 
         method = str(struct.unpack('=8s', request[:8])[0], encoding=ENCODING).strip('\x00')
 
-        if (method == Request.ENTER_CHAT.value):
+        if method == Request.ENTER_CHAT.value:
             user: User = unpack_user_message(request[8:])
             handle_connection_request(client_sock, user)
+        
+        elif method == Request.GET_USER.value:
+            user_name: str = unpack_get_user_request(request[8:])
+            handle_get_user_request(client_sock, user_name)
 
+        elif method == Request.CHECK_USER_CONNECTION.value:
+            user_name: str = unpack_check_user_request(request[8:])
+            validate_connection(user_name)
 
-        if request == 'list':
-            data = json.dumps(user_list, ensure_ascii=False)
-            client_sock.sendall(data.encode())
-                
-        client_sock.sendall(b'OK')
+        elif method == Request.LIST_USERS.value:
+            handle_get_list_request(client_sock)
         
 
 
